1418cd280157622422954cfe052f005494830c78
이재찬 <toxic023@naver.com>
2023-01-14 20:58:20 +0900

    init

---
+++ src/main/java/com/example/spj/SpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.spj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spj/SpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
