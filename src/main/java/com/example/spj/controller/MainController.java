3086a6ea2fec30f9fdba52b01dbfd32c630d9d56
FrenchRuin <toxic023@naver.com>
2023-01-15 16:05:01 +0900

    bootstrap setting

--- src/main/java/com/example/spj/controller/MainController.java
+++ src/main/java/com/example/spj/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.example.spj.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
     public String main(){
-        return "Hello";
+        return "index";
     }
 
 }
